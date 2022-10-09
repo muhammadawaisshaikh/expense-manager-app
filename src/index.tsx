5795e2f57d6805c9e89fcd6cb40b18de1222de95
Muhammad Awais <awaisshaikh94@gmail.com>
2022-10-09 23:23:05 +0500

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Fonts Added as Robotto Globally 
+// Fonts Added as Robotto Globally
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
