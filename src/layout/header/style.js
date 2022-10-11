35715b7e2a650a602d8d1230392050abf105470e
Muhammad Awais <awaisshaikh94@gmail.com>
2022-10-12 01:46:35 +0500

    done topbar

---
+++ src/layout/header/style.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@mui/styles';
+
+const Style = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    h1: {
+        margin: 0
+    },
+    avatar: {
+        backgroundColor: '#135ca3 !important'
+    }
+}));
+
+export default Style;
