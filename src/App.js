b4cbd006f7ba7c61919bf42348176f6cbb221b3a
Muhammad Awais <awaisshaikh94@gmail.com>
2022-10-12 01:17:45 +0500

    fixes from ts to js migration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>Expense Manager App</h1>
+      <Button variant="contained">Hello World</Button>
+    </header>
+  </div>
+  );
+}
+
+export default App;
